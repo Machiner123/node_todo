dab9023fe0eeccaed588aa58ebd4165219e74d26
unknown <igoratakhanov@gmail.com>
2017-12-04 15:34:58 -0500

    serving static files

--- app.js
+++ app.js
@@ -3,21 +3,23 @@ var express = require("express");
 var app = express()
 
 app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'))
+
 
 app.get('/', function(req, res){
-  // high level express function
-  res.sendFile(__dirname + '/index.html')
+  // high level express function takes car of piping, etc
+  res.render('index')
 })
 
 app.get('/contact', function(req, res){
-  // high level express function
-  res.sendFile(__dirname + '/contacts.html')
+  console.log(req.query)
+  res.render('contact')
 })
 
 app.get('/profile/:name', function(req, res){
   var data = {age: 29, job: 'ninja', hobbies: ['eating', 'fighting', 'running']}
-  // first argument to render is import, tells it which ejs file to pass
-  // second argument to
+  // first argument to render tells it which ejs file to pass
+  // second argument is an object that gets passed to the ejs file
   res.render('profile', {person: req.params.name, data: data})
 })
 
